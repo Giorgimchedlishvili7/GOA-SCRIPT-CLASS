01f17e870193a1a58544071c5785338c7e532e70
Giorgimchedlishvili7 <tiko2mariko@gmail.com>
2026-03-10 18:39:22 -0400

    commit

---
+++ level 045/homework/py.py
@@ -0,0 +1,17 @@
+numbers = [4, 7, 1, 9, 2]
+
+numbers.append(10)
+
+numbers.remove(7)
+
+numbers.pop(3)
+
+print(numbers)
+
+colors = ["red", "blue", "green", "yellow", "purple", "orange"]
+
+print(colors[:3])
+
+print(colors[4:6])
+
+print("My two favorite colors from this list are {colors[0]} and {colors[1]}")
