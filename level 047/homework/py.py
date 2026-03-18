c79722cb0f6699d2a616304bb027c96e88fe3638
Giorgimchedlishvili7 <tiko2mariko@gmail.com>
2026-03-18 17:23:36 -0400

    commit

---
+++ level 047/homework/py.py
@@ -0,0 +1,18 @@
+age = 17
+
+if age >= 18:
+        print("you can get a license")
+else:
+ print("No you cant get license your to young.")
+
+
+test_string = "HeLlO wOrLd 123! PyThOn"
+
+text = "'H', 'L', 'O', 'W', 'O', 'R', 'L', 'D', 'P', 'T', 'O', 'N'"
+
+upercased_letters = [text]
+
+upercased_letters.insert()
+
+print(upercased_letters)
+
