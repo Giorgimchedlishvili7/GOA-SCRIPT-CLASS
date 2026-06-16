392045e19e98d097a5b8aadef2972b5741d780e1
Giorgimchedlishvili7 <tiko2mariko@gmail.com>
2026-06-16 17:30:21 -0400

    COMMIT

---
+++ level 072/homework/py.py
@@ -0,0 +1,10 @@
+#list can add and removie tuples you cant add and remove
+
+tuple = (12, 45, 30, 85)
+
+first = tuple[0]
+last = tuple[3]
+
+middle_sum = tuple[1] + tuple[2]
+
+print("the two middle nums are", middle_sum)
